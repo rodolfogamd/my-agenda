91289e642569ca5136433fc085f988ebf67b1416
Jose Sal y Rosas <infojasyrc@gmail.com>
2016-03-21 22:58:33 -0500

    Modified production configuration to use sqlite for persistence.

--- agenda/settings/production.py
+++ agenda/settings/production.py
@@ -5,12 +5,8 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bd_prod',
-        'USER': 'admin',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
