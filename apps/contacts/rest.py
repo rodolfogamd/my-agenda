3d7d21c09c27af7a19486f726f23bfafbd1cffb2
Rodolfo Gamarra <rodolfogamd@gmail.com>
2016-07-26 10:06:46 -0500

    Contacts API: Added filter by: project_name and/or skill_name

--- apps/contacts/rest.py
+++ apps/contacts/rest.py
@@ -5,6 +5,9 @@ from rest_framework import routers, viewsets, filters
 from common.common import StandardResultsSetPagination
 from serializers import ContactSerializer
 from apps.contacts.models import Contact
+from apps.teams.models import Team
+from apps.levels.models import LevelSkill
+from django.db.models.query import QuerySet
 
 
 class ContactsFilter(django_filters.FilterSet):
@@ -19,3 +22,22 @@ class ContactsViewSet(viewsets.ModelViewSet):
     pagination_class = StandardResultsSetPagination
     filter_backends = (filters.DjangoFilterBackend,)
     filter_class = ContactsFilter
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if isinstance(queryset, QuerySet):
+            queryset = queryset.all()
+
+        project_name = self.request.query_params.get('project_name', None)
+        skill_name = self.request.query_params.get('skill_name', None)
+
+        if project_name is not None:
+            team_list = Team.objects.filter(project__name__icontains=project_name).values('contact_id')
+            queryset = queryset.filter(pk__in=team_list)
+
+        if skill_name is not None:
+            level_skill_list = LevelSkill.objects.filter(skill__name__icontains=skill_name).values('contact_id')
+            queryset = queryset.filter(pk__in=level_skill_list)
+
+        return queryset
